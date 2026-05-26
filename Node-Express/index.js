73e592d84146729f8a904667f6457ae46b66a68b
Hiral Pandey <188960430+hiralpandey1810@users.noreply.github.com>
2026-05-26 14:19:48 +0530

    Express Js Response method

--- Node-Express/index.js
+++ Node-Express/index.js
@@ -4,6 +4,14 @@ const app = express()
 app.listen(3000, ()=>{
     console.log('Successfully connected to port 3000');
 })
+app.set('view engine','ejs')
 app.get('/', (req, res)=>{
-    res.send("<h1>Hiiii World</h1>")
+    res.jsonp({ name: 'YahuBaba' , age: 25})
 })
+app.get('/about' , (req, res)=>{
+    res.redirect('..');
+})
+app.get('/user' , (req, res)=>{
+    res.render("user")
+})
+
